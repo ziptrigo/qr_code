c4dc4e5868ef0fe2edd4a5cf77c68c9d69aa3ec6
Joao Coelho <joaonc@hotmail.com>
2025-12-11 19:24:18 -0600

    boto3 client

---
+++ src/qr_code/common/aws.py
@@ -0,0 +1,33 @@
+import boto3
+
+
+def get_s3_client_with_role(
+    service: str, access_key: str, secret_key: str, role_arn: str, session_name: str = 'S3Session'
+):
+    """
+    Create an S3 client by assuming a role.
+
+    :param service: Service name, e.g. ``s3`` or ``ses``.
+    :param access_key: IAM user access key ID
+    :param secret_key: IAM user secret access key
+    :param role_arn: ARN of the role to assume
+    :param session_name: Name for the assumed role session
+
+    :returns: ``boto3`` client with assumed role credentials.
+    """
+    # Create STS client with user credentials
+    sts_client = boto3.client('sts', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+
+    # Assume the role
+    response = sts_client.assume_role(RoleArn=role_arn, RoleSessionName=session_name)
+
+    # Extract temporary credentials
+    credentials = response['Credentials']
+
+    # Create and return the client
+    return boto3.client(
+        service,
+        aws_access_key_id=credentials['AccessKeyId'],
+        aws_secret_access_key=credentials['SecretAccessKey'],
+        aws_session_token=credentials['SessionToken'],
+    )
